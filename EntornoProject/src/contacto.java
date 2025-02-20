008b75acb9e097f17ae4293ed6b1ed0636a2367a
Tortusito <alejandrito.vallin@gmail.com>
2025-02-20 12:14:46 +0100

    contactos

--- EntornoProject/src/contacto.java
+++ EntornoProject/src/contacto.java
@@ -1,3 +1,16 @@
+import java.time.LocalDate;
+
 public class contacto {
 
+    private String nombreContacto;
+    private String alias;
+    private int numTelefono;
+    private String correoElectronico;
+    private String tipoContacto;
+    private String infoTrabajo;
+    private LocalDate fechasImportantes;
+    private String relaciones;//padres,amigos(otros contactos)
+    private String notas;
+    private String sitioWeb;
+    
 }
