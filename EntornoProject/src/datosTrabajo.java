c8c175089b7ba90d5412a6111e11cd7c1fb45beb
felixvr2005 <felixvr2005@gmail.com>
2025-02-24 09:14:22 +0100

    .

--- EntornoProject/src/datosTrabajo.java
+++ EntornoProject/src/datosTrabajo.java
@@ -1,3 +1,50 @@
 public class datosTrabajo {
-
+    private direccion direccionEmpresa;
+    private String correoEmpresa;
+    private String numeroEmpresa;
+    private String numeroEmpleado;
+    private String correoEmpleado;
+    //CONSTRUCTOR VACIO
+    public datosTrabajo() {
+    }
+    //CONSTRUCTOR LLENO
+    public datosTrabajo(direccion direccionEmpresa, String correoEmpresa, String numeroEmpresa, String numeroEmpleado,
+            String correoEmpleado) {
+        this.direccionEmpresa = direccionEmpresa;
+        this.correoEmpresa = correoEmpresa;
+        this.numeroEmpresa = numeroEmpresa;
+        this.numeroEmpleado = numeroEmpleado;
+        this.correoEmpleado = correoEmpleado;
+    }
+    public direccion getDireccionEmpresa() {
+        return direccionEmpresa;
+    }
+    public void setDireccionEmpresa(direccion direccionEmpresa) {
+        this.direccionEmpresa = direccionEmpresa;
+    }
+    public String getCorreoEmpresa() {
+        return correoEmpresa;
+    }
+    public void setCorreoEmpresa(String correoEmpresa) {
+        this.correoEmpresa = correoEmpresa;
+    }
+    public String getNumeroEmpresa() {
+        return numeroEmpresa;
+    }
+    public void setNumeroEmpresa(String numeroEmpresa) {
+        this.numeroEmpresa = numeroEmpresa;
+    }
+    public String getNumeroEmpleado() {
+        return numeroEmpleado;
+    }
+    public void setNumeroEmpleado(String numeroEmpleado) {
+        this.numeroEmpleado = numeroEmpleado;
+    }
+    public String getCorreoEmpleado() {
+        return correoEmpleado;
+    }
+    public void setCorreoEmpleado(String correoEmpleado) {
+        this.correoEmpleado = correoEmpleado;
+    }
+    
 }
